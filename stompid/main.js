a4633af56cc8bb6c5f127e97515e8b37bed761c3
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-05-18 20:53:33 +0800

    Update main.js

--- stompid/main.js
+++ stompid/main.js
@@ -1,5 +1,16 @@
 let repeat = 2
-setInterval(() => {
+let interval_cache
+let interval = Number(document.querySelector(".interval").value)
+const loop = () => {
     repeat += 1
     document.querySelector(".main").innerHTML = `y${"oy".repeat(repeat)}jaj is s<b>tom</b>pid`
-}, 500)
+}
+let id = setInterval(loop, 500)
+setInterval(() => {
+    interval = Number(document.querySelector(".interval").value)
+    if (interval != interval_cache) {
+        interval_cache = interval
+        clearInterval(id)
+        id = setInterval(loop, interval_cache)
+    }
+}, 16.66666666666666666666666666)
